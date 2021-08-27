d61cd230fd6d17f8abf5be72fbf86fdbc1c59b1f
Mogakamo <mogaka.amo254@gmail.com>
2021-08-27 10:51:46 +0300

    finished building a single file component

--- twotter/src/views/Home.vue
+++ twotter/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} <strong>Followers: </strong> {{ followers }}
+    <button @click="followUser">Follow</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "m_ogakadev",
+        firstName: "Amos",
+        lastName: "Nyaburi",
+        email: "com02519@uoeld.ac.ke",
+        isAdmin: true,
+      },
+    };
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.ueser.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+
+  mounted() {
+    this.followUser();
+  },
+};
 </script>
